c356aa9db8e8fb2b03c41a4af73f086a3ce4ba54
Giovann Wah <giovan92@uga.edu>
2017-08-09 02:20:20 -0400

    Added csv handler to library

---
+++ src/main/java/data/InputProvider.java
@@ -0,0 +1,12 @@
+package data;
+
+import com.github.neuralnetworks.architecture.NeuralNetwork;
+import com.github.neuralnetworks.architecture.NeuralNetworkImpl;
+import com.github.neuralnetworks.training.TrainingInputProvider;
+
+public class InputProvider implements TrainingInputProvider {
+
+    public static void main(String[] args) {
+        System.out.println("Test.");
+    }
+}
