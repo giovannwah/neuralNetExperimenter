d92298733abc224a5f80570236ecf219ab136faa
Giovann Wah <giovan92@uga.edu>
2017-08-10 12:55:13 -0400

    Began Custom Input Provider work

--- src/main/java/data/CustomInputProvider.java
+++ src/main/java/data/CustomInputProvider.java
@@ -4,9 +4,17 @@ import com.github.neuralnetworks.architecture.NeuralNetwork;
 import com.github.neuralnetworks.architecture.NeuralNetworkImpl;
 import com.github.neuralnetworks.training.TrainingInputProvider;
 
-public class InputProvider implements TrainingInputProvider {
+import wah.giovann.csvhandler.CSVReader;
+import wah.giovann.csvhandler.CSVFileFormat;
+import wah.giovann.csvhandler.CSVArray;
+
+public class CustomInputProvider implements TrainingInputProvider {
 
     public static void main(String[] args) {
         System.out.println("Test.");
+    }
+
+    public void reset() {
+
     }
 }
